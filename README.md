f2642241095f57554f206cc6ae86f2c72a2c091b
Felipe Fernandes <mewbi.fernandes@gmail.com>
2022-09-11 20:35:16 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Scientific Research
+
+This repository will store some stuff about my scientific research.
+
+Some codes and notes will not have a previous context.
+
+## Thematic
+
+The main thematic is Geometric Optimization.
+
+My notes could be found [here](https://energetic-blinker-147.notion.site/IC-5f13b1deac5f4073808ff39dc48a2302)
---
+++ newtons-method.py
@@ -0,0 +1,30 @@
+# Simple implemation of newtons method to find root of a function
+# Uses only the concept function and your derivate
+def f(x): # Example function
+    return x**2 - x - 1
+
+def fp(x): # f'(x)
+    return 2*x - 1
+
+def newthon_step(f, fp, x):
+    return x - f(x)/fp(x)
+
+def newthon_step2(f, x1, x2):
+    # Secant method
+    return x1 - f(x1)*((x1-x2)/(f(x1)-f(x2)))
+
+def newtons_method(f, fp, start, n = 10, tolerance=1e-10):
+    prev = start
+    for _ in range(n):
+        #new = newthon_step(f, fp, prev)
+        new = newthon_step2(f, prev, prev+1)
+        if abs(f(new)) < tolerance:
+            return new
+        prev = new
+    raise Exception("Newton's Method didn't converge.")
+
+try:
+    c = newtons_method(f, fp, 1, 1000)
+    print("Converge in {}".format(c))
+except Exception as e:
+    print(e)
